72c24d1e89cd502106ae200a0695d0e054a93ed2
Ahmad W.R <wageh4568@gmail.com>
2023-12-19 16:19:53 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const azzaih = require("./models/azzaih");
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
+// auto Refresh
+
+const path = require("path");
+const livereload = require("livereload");
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname, 'public'));
+
+
+const connectLivereload = require("connect-livereload");
+app.use(connectLivereload());
+
+liveReloadServer.server.once("connection", () => {
+  setTimeout(() => {
+    liveReloadServer.refresh("/");
+  }, 100);
+});
+
+// mongoDB
+
+const mongoose = require('mongoose');
+
+mongoose.connect("mongodb+srv://Ahmad_RAQ:1w3r5y7i8@cluster0.wrxv6um.mongodb.net/")
+  .then(result => {
+    app.listen(port, () => {
+      console.log(`Example app listening on http://localhost:${port}/`)
+    })
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+//pages
+
+//index
+
+app.get('/', (req, res) => {
+  res.render("index")
+})
+
+//mnuat
+
+app.get("/mnuat", (req, res) => {
+  azzaih.find()
+    .then((result) => {
+      res.render("mnuat", { item: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+})
+
+//support
+
+app.get("/support", (req, res) => {
+  res.render("support")
+})
