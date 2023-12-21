4b95f36eed985f02c41b693503e164b92a659b71
Ahmad W.R <wageh4568@gmail.com>
2023-12-21 19:14:57 +0300

    n

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3000 || process.env.PORT
 const azzaih = require("./models/azzaih");
 
 app.use(express.static('public'))
