cc4e2404e773be21f3fd8994618c3383beb76e68
Ahmad6556 <wageh4568@gmail.com>
2024-03-27 06:51:09 +0300

    3.1.2

--- public/showfuv.js
+++ public/showfuv.js
@@ -15,7 +15,7 @@ function azzaihf(URL, U_s, N_s) {
     </button>
     <br>
     <link rel="stylesheet" href="/iframe.css">
-    <iframe id="yyy b${U_s}" src="${URL}" title="يا عباس | إصدار يا محرم | حسين فيصل | محرم 1438" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+    <iframe id="yyy" src="${URL}" title="يا عباس | إصدار يا محرم | حسين فيصل | محرم 1438" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
     </iframe>`;
 }
 
